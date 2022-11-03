e05bf5cf43f47e23c6af60572072a588d99dd628
marat-b <buran17@mail.ru>
2022-11-03 12:18:30 +0500

    adjust_layers

--- augm_method_jpegcompression.py
+++ augm_method_jpegcompression.py
@@ -10,7 +10,28 @@ from imgaug import augmenters as iaa
 import numpy
 from tqdm import tqdm
 
-# form image files with 4 channels
+# for image files with 4 channels
+
+def adjust_layers(image):
+    blue, green, red, alpha = cv2.split(image)
+    # new_alpha = alpha.copy()
+    new_alpha = np.where(alpha == 255, alpha, 0)
+    # new_alpha = np.where(alpha == 0, alpha, 255)
+    # cv2_imshow(new_alpha, 'new_alpha')
+    blue = cv2.bitwise_and(blue, blue, mask=new_alpha)
+    # cv2_imshow(blue, 'blue')
+    blue = np.where(blue > 0, blue, 255)
+    # cv2_imshow(blue, 'blue')
+    green = cv2.bitwise_and(green, green, mask=new_alpha)
+    green = np.where(green > 0, green, 255)
+    # cv2_imshow(green, 'green')
+    red = cv2.bitwise_and(red, red, mask=new_alpha)
+    red = np.where(red > 0, red, 255)
+    # cv2_imshow(red, 'red')
+    new_image = cv2.merge((blue, green, red, new_alpha))
+    # cv2_imshow(new_image[:, :, ::-1], 'new_image')
+    # cv2_imshow(new_image, 'new_image')
+    return new_image
 
 if __name__ == '__main__':
     import argparse
@@ -56,16 +77,18 @@ if __name__ == '__main__':
 
         old_image = imageio.imread(join(input_dir, file))
         for i in range(len(augments)):
-            old_image = iaa.Resize(0.5)(image=old_image)
+            # old_image = iaa.Resize(0.5)(image=old_image)
+            # old_image = adjust_layers(old_image)
             mask = old_image[:,:,[3]]
             # print(f'mask.shape={mask.shape}')
-            aug = augments[i]
+            aug = augments2[i]
             new_image = aug(image=old_image[:,:,:3])
             # new_image = iaa.Resize(0.5)(image=new_image)
-            new_image = augments2[i](image=new_image)
-            # print(f'new_image.shape={new_image.shape}')
+            # new_image = augments2[i](image=new_image)
+            # print(f'new_image.shape={new_image.shape}, mask.shape={mask.shape}')
             # new_image = iaa.Resize(4.0)(image=new_image)
-            new_image = cv2.merge((new_image, mask))
+            # new_image = cv2.merge((new_image, mask))
+            new_image = np.concatenate((new_image, mask), axis=2)
             if suffix is None:
                 imageio.imwrite('{}.{}'.format(join(output_dir, file_name), ext), new_image)
             else:
